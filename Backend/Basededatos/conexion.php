7dfcc2370042b16a2c91499e74851d6136ead0e0
Dave Gómez <84810204+Dave-Gom@users.noreply.github.com>
2022-02-27 19:23:54 -0300

    Update conexion.php

--- Backend/Basededatos/conexion.php
+++ Backend/Basededatos/conexion.php
@@ -1,9 +1,6 @@
 <?php
     $conection = mysqli_connect(
-        'localhost',
-        'root',
-        '',
-        'chaco_matriz'
+        
     );
     if(!$conection)
         die (json_encode("{error: 'No se pudo conectar a la Base de Datos'"));
